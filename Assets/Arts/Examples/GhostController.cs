73ed2dba2110a4ed9f6491bd31a0ac1541a5e0ba
SHADOW_PC\xianz <xianze.jiang@gmail.com>
2026-05-18 01:29:55 +0800

    feat: update Ghost material, controller, and scene

--- Assets/Arts/Examples/GhostController.cs
+++ Assets/Arts/Examples/GhostController.cs
@@ -5,22 +5,33 @@ using UnityEngine.UI; //引用UGUI代码库
 
 public class GhostController : MonoBehaviour
 {
+    #region [成员变量]
     public Button SetColorBtn;
     public GameObject character;
     public Slider RotateSlider;
+    public Slider RepeatXSlider;
+    
+    private Material CharacterMat;
+    #endregion
+
+    #region [Start]
     void Start()
     {
         SetColorBtn.onClick.AddListener(OnSetColor);
         RotateSlider.onValueChanged.AddListener(OnRotateSlider);
+        RepeatXSlider.onValueChanged.AddListener(OnRepeatXSlider);
+        CharacterMat = character.GetComponentInChildren<SkinnedMeshRenderer>().sharedMaterial;
     }
+    #endregion
 
+    #region [交互实现]
     // Update is called once per frame
     void OnSetColor()
     {
         float r = Random.value;
         float g = Random.value;
         float b = Random.value;
-        character.GetComponentInChildren<SkinnedMeshRenderer>().sharedMaterial.SetColor("_FresnelColor", new Color(r,g,b,1));
+        CharacterMat.SetColor("_FresnelColor", new Color(r,g,b,1));
     }
 
     
@@ -28,4 +39,11 @@ public class GhostController : MonoBehaviour
     {
         character.GetComponent<RotateSelf>().speed = value;
     }
+
+    void OnRepeatXSlider(float value)
+    {
+        Vector4 _animation = CharacterMat.GetVector("_Animation");
+        CharacterMat.SetVector("_Animation", new Vector4(value, _animation.y, _animation.z, _animation.w));
+    }
+    #endregion
 }
